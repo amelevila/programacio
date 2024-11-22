f38ced9db6e66886d50fdecfd14eeea046706280
adria mele <amelevila78@gmail.com>
2024-11-22 13:28:25 +0100

    castellers

--- joel/castellers.java
+++ joel/castellers.java
@@ -4,20 +4,26 @@ public class castellers {
 
   static Scanner in;
 
-  public static void casoDePrueba() {
-
-    
-      
-  } // casoDePrueba
-
   public static void main(String[] args) {
 
     in = new java.util.Scanner(System.in);
+    
+    int rondes = in.nextInt();
 
-    int numCasos = in.nextInt();
-    for (int i = 0; i < numCasos; i++)
-      casoDePrueba();
+    int[] num = new int [8];
+    int boomers, milenial, a, b;
 
-  } // main
+    for (int r = 0; r<rondes; r++) {
+        for (int i = 0; i<8; i++) {
+            num[i] = in.nextInt();          
+        }
+
+        boomers = num[1] - num[0] + num[4] - num[5];
+        milenial = num[2] - num[3] + num[6] - num[7];
+        a = num[1] - num[0] + num[4] - num[5];
+        b = num[1] - num[0] + num[4] - num[5];
+    }
+
+  }
 
 }
