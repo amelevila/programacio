34a86f24ca5c7336a5d2e01b5f31bd4f52ced35e
adria mele <amelevila78@gmail.com>
2024-12-05 10:53:00 +0100

    artmodern

--- joel/artModernLent.java
+++ joel/artModernLent.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class artModern {
+public class artModernLent {
 
     static Scanner in;
 
@@ -11,32 +11,28 @@ public class artModern {
         int m = in.nextInt();
         int n = in.nextInt();
         int k = in.nextInt();
-        int[] rows = new int [m];
-        int[] cols = new int [n];
+        boolean[][] taula = new boolean [m][n];
 
         for (int t = 0; t<k; t++) {
             char elec = in.next().charAt(0);
             int num = in.nextInt();
             if (elec == 'R') {
-                rows[num-1]++;
+                for (int i = 0; i<n; i++) {
+                    taula[num-1][i] = !taula[num-1][i];
+                }
             }
             else {
-                cols[num-1]++;
+                for (int i = 0; i<m; i++) {
+                    taula[i][num-1] = !taula[i][num-1];
+                }
             }
         }
         
         int count = 0;
-        //char[][] taula = new char [m][n];
         for (int i = 0; i<m; i++) {
             for (int j = 0; j<n; j++) {
-                if ((rows[i]+cols[j])%2 != 0) {
-                    count++;
-                    //taula[i][j] = 'D';
-                }
-                //else taula[i][j] = 'N';
-                //System.out.print(taula[i][j]);
+                if (taula[i][j]) count++;
             }
-            //System.out.println();
         }
 
         System.out.println(count);
