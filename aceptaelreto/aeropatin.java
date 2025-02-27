058c1209683721f94d87dba8c44c30fe3192f4df
adria mele <amelevila78@gmail.com>
2025-02-27 11:51:46 +0100

    no completat aeropatin

--- aceptaelreto/aeropatin.java
+++ aceptaelreto/aeropatin.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.TreeMap;
 
 public class aeropatin {
    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -10,54 +9,21 @@ public class aeropatin {
          table[i] = reader.readLine().toCharArray();
       }
 
-      TreeMap<String,String> map = new TreeMap<>();
-      String key = "0/0";
-      int[] pos = {0,0};
+      boolean[][] pos = new boolean[n][m];
+      pos[0][0] = true;
+      
+      /* 
+            if (pos[x][y]) {
+               if (y != m-1 && table[i][y+1] != 'X') pos[i][y+1] = true;
+               if (y != 0 && table[i][y-1] != 'X') pos[i][y-1] = true;
 
-      while (!(pos[0] == n-1 && pos[1] == m-1)) {
-         if (!map.containsKey(key)) map.put(key, "");
+               if (i != n-1  && table[i+1][y] != 'X') pos[i+1][y] = true;
+               if (i != 0 && table[i-1][y] != 'X') pos[i-1][y] = true;
+            } 
+      */
 
-         //System.out.println(map);
-
-         if (!map.get(key).contains("e")) {
-            map.put(key, map.get(key)+"e");
-            if (pos[1] == m-1 || table[pos[0]][pos[1]+1] == 'X') continue;
-            
-            pos[1]++;
-            key = String.format("%d/%d", pos[0], pos[1]);
-         }
-
-         else if (!map.get(key).contains("s")) {
-            map.put(key, map.get(key)+"s");
-            if (pos[0] == n-1  || table[pos[0]+1][pos[1]] == 'X') continue;
-
-            pos[0]++;
-            key = String.format("%d/%d", pos[0], pos[1]);
-         }  
-         
-         else if (!map.get(key).contains("n")) {
-            map.put(key, map.get(key)+"n");
-            if (pos[0] == 0 || table[pos[0]-1][pos[1]] == 'X') continue;
-            
-            pos[0]--;
-            key = String.format("%d/%d", pos[0], pos[1]);
-         }
-
-         else if (!map.get(key).contains("w")) {
-            map.put(key, map.get(key)+"w");
-            if (pos[1] == 0 || table[pos[0]][pos[1]-1] == 'X') continue;
-            
-            pos[1]--;
-            key = String.format("%d/%d", pos[0], pos[1]);
-         }
-         
-         else {
-            System.out.println("NO");
-            return;
-         }
-
-      }
-      System.out.println("SI");
+      String result = (pos[n-1][m-1]) ? "SI":"NO";
+      System.out.println(result);
       
    } 
    
