90bf89b4071d97260cb425f6fc707b040bb81f2c
adria mele <amelevila78@gmail.com>
2025-02-27 12:31:00 +0100

    seHaColado plantilla

---
+++ aceptaelreto/seHaColado.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class seHaColado {
+   static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+   public static void casoPrueba() throws IOException {
+
+      int t = Integer.parseInt(reader.readLine());
+      for (int i = 0; i < t; i++) {
+         
+         
+
+      }
+   } 
+   
+   public static void main(String[] args) throws IOException {
+      int t = Integer.parseInt(reader.readLine());
+      for (int i = 0; i < t; i++) {
+         casoPrueba();
+      }
+   }
+    
+}
