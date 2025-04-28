11982140165ce2b85cd8a8abec12552ae15ba4b1
adria mele <amelevila78@gmail.com>
2025-04-28 21:21:45 +0200

    CollectingNumber.java start

---
+++ cses/CollectingNumber.java
@@ -0,0 +1,85 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+ 
+public class CollectingNumber {
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+ 
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+ 
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+ 
+        @SuppressWarnings("ConvertToTryWithResources")
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            }  while ((c = read()) >= '0' && c <= '9');
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+    }
+    static Reader in = new Reader();
+    public static void main(String[] args) throws IOException {
+        int n = in.nextInt();
+ 
+        Set<Integer> nums = new LinkedHashSet<>();
+        for (int i = 0; i<n; i++) {
+            int value = in.nextInt();
+            nums.add(value);
+        }
+
+        int rounds = 0;
+        
+        while (!nums.isEmpty()) {
+            int first = nums.iterator().next();
+            System.out.println(first);
+            for (int num : nums) {
+                if (num>first) {
+                    nums.remove(first);
+                    first = num;
+                }
+                System.out.println(nums);
+            }
+            rounds++;
+        }
+        System.out.println(rounds);
+    }
+}
